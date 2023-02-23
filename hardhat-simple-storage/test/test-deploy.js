33b6e2cbe0057386e648d26f029ca1907cae3a08
karlMoreno <karlcmoreno0811@gmail.com>
2023-02-23 12:55:04 -0800

    Expected Value Test

---
+++ hardhat-simple-storage/test/test-deploy.js
@@ -0,0 +1,18 @@
+const { ethers } = require("hardhat")
+const { expect,assert } = require("chai")
+
+describe("SimpleStorage", () => {
+    let simpleStorageFactory, simpleStorage
+    beforeEach(async function () {
+       simpleStorageFactory = await ethers.getContractFactory("SimpleStorage")
+       simpleStorage = await simpleStorageFactory.deploy()
+    })
+
+    it("Should start with a favorite number of 0", async function () {
+        const currentValue = await simpleStorage.retrieve()
+        const expectedValue = "0"
+        // expect
+        // assert
+        assert.equal(currentValue.toString(), expectedValue)
+    })
+})
