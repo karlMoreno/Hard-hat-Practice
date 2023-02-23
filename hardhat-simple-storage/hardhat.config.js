8e9aeeec96665c4823faa07c7e50b7635f49388f
karlMoreno <karlcmoreno0811@gmail.com>
2023-02-23 09:45:45 -0800

    Connecting to Hardhat Local Network

--- hardhat-simple-storage/hardhat.config.js
+++ hardhat-simple-storage/hardhat.config.js
@@ -1,11 +1,19 @@
 require("@nomicfoundation/hardhat-toolbox")
 require("@nomiclabs/hardhat-etherscan")
+require("./tasks/block-number")
 
 /** @type import('hardhat/config').HardhatUserConfig */
 const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY
 
 module.exports = {
     defaultNetwork: "hardhat",
+    networks: {
+      localhost: {
+        url: "http://127.0.0.1:8545/",
+      // accounts already has
+      chainId: 31337,
+      }
+    },
     solidity: "0.8.17",
     etherscan: {
         apiKey: ETHERSCAN_API_KEY,
