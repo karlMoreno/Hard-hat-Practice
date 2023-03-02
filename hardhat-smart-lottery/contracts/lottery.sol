023170b763bbd4bd694c65f42e35669f8d62b345
karlMoreno <karlcmoreno0811@gmail.com>
2023-03-01 19:47:51 -0800

    Lottery smart contract functionality

--- hardhat-smart-lottery/contracts/lottery.sol
+++ hardhat-smart-lottery/contracts/lottery.sol
@@ -1,8 +1,48 @@
 // SPDX-License-Identifier: SEE LICENSE IN LICENSE
 pragma solidity ^0.8.7;
 
-contract Raffle {
-    constructor() {
+import "@chainlink/contracts/src/v0.8/VRFConsumerBaseV2.sol";
+
+error Raffle__NotEnoughEthEntered();
+
+contract Raffle is VRFConsumerBaseV2{
+    // State Variables
+    uint256 private immutable i_entranceFee;
+    address[] private s_players;
+
+    // Events
+    event RaffleEnter(address indexed player);
+
+    constructor(address vrfCoordinatorV2, uint256 entranceFee) VRFConsumerBaseV2(vrfCoordinatorV2){
+        i_entranceFee = entranceFee;
+    }
+
+    function enterRaffle() public payable {
+        // msg.val >= i_entrancefee
+        if (msg.value < i_entranceFee) {
+            revert Raffle__NotEnoughEthEntered();
+        }
+        s_players.push(payable(msg.sender));
+        emit RaffleEnter(msg.sender);
+    }
+
+    function requestRandomWinner() external{
         
     }
+
+    function fulfillRandomWords(uint256 requestId, uint256[] memory randomWords) internal override{
+
+    }
+
+
+    // View / Pure Functions
+    function getEntranceFee() public view returns (uint256){
+        return i_entranceFee;
+    }
+
+    function getPlayer(uint256 index) public view returns (address) {
+        return s_players[index];
+    }
+
+
 }
