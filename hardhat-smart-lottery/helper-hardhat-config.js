194370f65d6c92b1e04fe89785254c8bc6140326
karlMoreno <karlcmoreno0811@gmail.com>
2023-03-03 09:26:19 -0800

    VRF COORDINATOR V2 mock

--- hardhat-smart-lottery/helper-hardhat-config.js
+++ hardhat-smart-lottery/helper-hardhat-config.js
@@ -4,3 +4,10 @@ const networkConfig = {
         vrfCoordinatorV2:"0x2Ca8E0C643bDe4C2E08ab1fA0da3401AdAD7734D"
     }
 }
+
+const developmentChains = ["hardhat", "localhost"]
+
+module.exports = {
+    networkConfig,
+    developmentChains
+}
