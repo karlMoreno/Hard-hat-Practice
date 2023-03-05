55a7971e385278e7b8786437300c803c7f5ec1d2
karlMoreno <karlcmoreno0811@gmail.com>
2023-03-04 16:28:20 -0800

    Deploy script

--- hardhat-smart-lottery/scripts/01-deploy-lottery.js
+++ hardhat-smart-lottery/scripts/01-deploy-lottery.js
@@ -1,12 +1,33 @@
-const { getNamedAccounts, network } = require("hardhat")
+const { getNamedAccounts, network, ethers } = require("hardhat")
+const { developmentChains, networkConfig } = require("../helper-hardhat-config")
+
+const VRF_SUB_FUND_AMOUNT = ethers.utils.parseEther("2")
 
 module.exports = async function ({getnamedAccounts, deployments}) {
     const {deploy, log} = deployments
     const {deployer} = await getNamedAccounts()
+    let vrfCoordinatorV2Address
 
+    if (developmentChains.includes(network.name)) {
+        const vrfCoordinatorV2Mock = await ethers.getContractAt("VRFCoordinatorV2Mock")
+        vrfCoordinatorV2Address = vrfCoordinatorV2Mock.address
+        const transactionResponse = await vrfCoordinatorV2Mock
+        const transactionReceipt = await transactionResponse.wait(1)
+        subscriptionId = transactionReceipt.events[0].args.subId
+        //usually need link token on a real network
+        await vrfCoordinatorV2Mock.fundSubscription(subscriptionId)
+    } else {
+        vrfCoordinatorV2Address = networkConfig[chainId]["vrfCoordinatorV2"]
+    }
+    const entraceFee = networkConfig[chainId]["entranceFee"]
+    const gasLane = networkConfig[chainId]["gasLane"]
+    const callbackGasLimit = networkConfig[chainId]["callbackGasLimit"]
+    const interval = networkConfig[chainId]["interval"]
+
+    const args = [vrfCoordinatorV2Address, entraceFee, gasLane,callbackGasLimit, interval]
     const raffle = await deploy("lottery",{
         from: deployer,
-        args: [],
+        args: args,
         log:true,
         waitConfirmations: network.config.blockConfirmations || 1,
 
