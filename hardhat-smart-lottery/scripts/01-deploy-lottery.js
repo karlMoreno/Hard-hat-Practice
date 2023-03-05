b1465998a9a3bd038c21f0db55c1a43664405c3e
karlMoreno <karlcmoreno0811@gmail.com>
2023-03-04 16:42:06 -0800

    Deploy script ready

--- hardhat-smart-lottery/scripts/01-deploy-lottery.js
+++ hardhat-smart-lottery/scripts/01-deploy-lottery.js
@@ -1,5 +1,6 @@
 const { getNamedAccounts, network, ethers } = require("hardhat")
 const { developmentChains, networkConfig } = require("../helper-hardhat-config")
+const { verify } = require("../helper-hardhat-config")
 
 const VRF_SUB_FUND_AMOUNT = ethers.utils.parseEther("2")
 
@@ -32,4 +33,12 @@ module.exports = async function ({getnamedAccounts, deployments}) {
         waitConfirmations: network.config.blockConfirmations || 1,
 
     })
+
+    if(!developmentChains.includes(network.name) && process.env.ETHERSCAN_API_KEY) {
+        log("Verifying...")
+        await verify(raffle.address, args)
+    }
+    log("------------------------------------------------------------")
 }
+
+module.exports.tag = ["all","raffle"]
