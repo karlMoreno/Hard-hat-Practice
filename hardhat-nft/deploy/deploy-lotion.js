6a43e5cb887d56863ff0b3ab061b1585c06432ab
karlMoreno <karlcmoreno0811@gmail.com>
2023-03-05 15:34:55 -0800

    Deploy lotion

--- hardhat-nft/deploy/deploy-lotion.js
+++ hardhat-nft/deploy/deploy-lotion.js
@@ -1,27 +1,17 @@
-const hre = require("hardhat");
-const fs = require("fs");
+const { ethers } = require("hardhat");
 
-async function deployContract() {
-  const CONTRACT_NAME = "Lotion.sol";
-  const [deployer] = await hre.ethers.getSigners();
-  console.log("Deploying contract with the account:", deployer.address);
+async function main() {
+  const LotionNFT = await ethers.getContractFactory("LotionNFT");
+  const lotionNFT = await LotionNFT.deploy();
 
-  const contractFactory = await hre.ethers.getContractFactory(CONTRACT_NAME);
-  const contract = await contractFactory.deploy();
-  await contract.deployed();
-  console.log(`${CONTRACT_NAME} deployed to address: ${contract.address}`);
+  await lotionNFT.deployed();
 
-  // Replace with your own lotion name, description, and ingredients
-  const name = "My Lotion";
-  const description = "This is a description of my lotion";
-  const ingredients = [
-    { wallet: "0x1234567890123456789012345678901234567890", name: "Ingredient 1" },
-    { wallet: "0x2345678901234567890123456789012345678901", name: "Ingredient 2" },
-    { wallet: "0x3456789012345678901234567890123456789012", name: "Ingredient 3" },
-  ];
-
-  const tx = await contract.createLotion(name, description, ingredients);
-  console.log(`Lotion NFT created successfully with transaction hash: ${tx.hash}`);
+  console.log("LotionNFT contract deployed to:", lotionNFT.address);
 }
 
-deployContract();
+main()
+  .then(() => process.exit(0))
+  .catch(error => {
+    console.error(error);
+    process.exit(1);
+  });
